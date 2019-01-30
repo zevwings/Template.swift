3e86683fa96385554368565383deffafa54160a1
zevwings <zev.wings@gmail.com>
2019-01-30 15:29:25 +0800

    1. 添加分页请求模板 2. 修改网络请求模板

--- zevwings/File/Paginator Request.xctemplate/___FILEBASENAME___.swift
+++ zevwings/File/Paginator Request.xctemplate/___FILEBASENAME___.swift
@@ -3,12 +3,20 @@
 import HTTPKit
 import Result
 
-public struct ___FILEBASENAMEASIDENTIFIER___ : Request {
+public struct ___FILEBASENAMEASIDENTIFIER___ : Request, Paginator {
     
     public typealias Service = <#Serviceable#>
     
     public typealias Model = <#ModelType#>
         
+    /// 当前页码，默认为0
+    public var index: Int = 0
+    
+    /// 从返回值中获取数组元素对应的Key
+    public var elementKey: String {
+        return <#elementKey#>
+    }
+    
     /// 基础路径
     public var service: Service {
         return <#.application#>
@@ -29,13 +37,15 @@ public struct ___FILEBASENAMEASIDENTIFIER___ : Request {
         return <#.requestParameters(parameters: <#parameters#>, formatter: .json)#>
     }
     
-    /** 请求头设置，默认为空，详细内容请阅读 `Request` 部分
+    /** 设置页码对应的参数Key，默认为`page`，详细内容请阅读 `Paginator` 部分
+    /// 请求头设置，默认为空
     public var headerFields: [String : String] {
         return [:]
     }
     */
     
-    /** 校验类型，校验返回的 status code 是否为正确的值，默认校验正确和重定向code，详细内容请阅读 `Request` 部分
+    /** 设置页码对应的参数Key，默认为`page`，详细内容请阅读 `Paginator` 部分
+    /// 校验类型，校验返回的 status code 是否为正确的值，默认校验正确和重定向code
     public var validationType: ValidationType {
         return .successAndRedirectCodes
     }
@@ -45,11 +55,28 @@ public struct ___FILEBASENAMEASIDENTIFIER___ : Request {
     public var transformer: Transformer? {
         return JSONTransformer()
     }
-
+    
+    /** 设置页码对应的参数Key，默认为`page`，详细内容请阅读 `Paginator` 部分
+    public var indexKey: String {
+        return "page"
+    }
+    */
+    
+    /** 设置每页数据数量，默认20，详细内容请阅读 `Paginator` 部分
+    public var count: Int {
+        return 20
+    }
+    */
+    
+    /** 设置每页数据数量对应的Key，默认为`rows`，详细内容请阅读 `Paginator` 部分
+    public var countKey: String {
+        return "rows"
+    }
+    */
 }
 
 /** 请求拦截器，详细内容请阅读 `Interceptor` 部分
-
+ 
 extension ___FILEBASENAMEASIDENTIFIER___ : Interceptor {
     
     /// 拦截请求参数，可以在生成`URLRequest`之前，对参数进行修改
@@ -85,3 +112,4 @@ extension ___FILEBASENAMEASIDENTIFIER___ : Interceptor {
 */
 
 extension ___FILEBASENAMEASIDENTIFIER___ : Equatable { }
+
