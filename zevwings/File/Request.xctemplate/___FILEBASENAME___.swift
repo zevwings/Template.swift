6b27ce7a9f3c536ef21b188e059a7ff29d1c4cb1
zevwings <zev.wings@gmail.com>
2019-11-08 16:30:29 +0800

    update templet

--- zevwings/File/Request.xctemplate/___FILEBASENAME___.swift
+++ zevwings/File/Request.xctemplate/___FILEBASENAME___.swift
@@ -1,87 +1,39 @@
 //___FILEHEADER___
-
+import Foundation
 import HTTPKit
-import Result
 
-public struct ___FILEBASENAMEASIDENTIFIER___ : Request {
+enum ___FILEBASENAMEASIDENTIFIER___ {
     
-    public typealias Service = <#Serviceable#>
-    
-    public typealias Model = <#ModelType#>
-        
+    case <#Node#>
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___ : Request {
+ 
+    typealias Service = NetwrokService
+
     /// 基础路径
-    public var service: Service {
-        return <#.application#>
+    var service: Service {
+        return .default
     }
-    
-    /// 请求路径
-    public var path: String {
-        return <#path#>
+
+    var route: Route {
+        switch self {
+        case <#Node#>:
+            return <#ROUTE#>
+        }
     }
-    
-    /// 请求方法
-    public var method: HTTPMethod {
-        return <#.post#>
-    }
-    
+
     /// 请求内容
-    public var content: Content {
-        return <#.requestParameters(parameters: <#parameters#>, formatter: .json)#>
-    }
-    
-    /** 请求头设置，默认为空，详细内容请阅读 `Request` 部分
-    public var headerFields: [String : String] {
-        return [:]
-    }
-    */
-    
-    /** 校验类型，校验返回的 status code 是否为正确的值，默认校验正确和重定向code，详细内容请阅读 `Request` 部分
-    public var validationType: ValidationType {
-        return .successAndRedirectCodes
-    }
-    */
-    
-    /// 数据转换器，默认为`nil`
-    public var transformer: Transformer? {
-        return JSONTransformer()
+    var content: Content {
+        switch self {
+        case <#Node#>:
+            return <#Content#>
+        }
     }
 
-}
-
-/** 请求拦截器，详细内容请阅读 `Interceptor` 部分
-
-extension ___FILEBASENAMEASIDENTIFIER___ : Interceptor {
-    
-    /// 拦截请求参数，可以在生成`URLRequest`之前，对参数进行修改
-    public func intercept(paramters: Parameters?) throws -> Parameters? {
-        return parameters
-    }
-    
-    /// 拦截网络请求，可以在发起网络请求之前，对`URLRequest`进行修改
-    public func intercept(urlRequest: URLRequest) throws -> URLRequest {
-        return urlRequest
-    }
-    
-    /// 拦截网络请求结果，可以在反序列化结果之前对返回结果`Response`进行修改
-    public func intercept(response: Response) throws -> Response {
-        return response
-    }
-    
-    /// 网络请求将要发送
-    public func willSend<R>(_ urlRequest: URLRequest, request: R) where R : Request {
-        
-    }
-    
-    /// 网络请求进度回调
-    public func process<R>(_ progress: Progress, request: R) where R : Request {
-        
-    }
-    
-    /// 网络请求收到结果
-    public func didReceive<R>(_ result: Result<Response, HTTPError>, request: R) where R : Request {
-        
+    var transformer: Transformer? {
+        return ModelTransformer()
     }
 }
-*/
 
 extension ___FILEBASENAMEASIDENTIFIER___ : Equatable { }
